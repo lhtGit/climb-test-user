de0e73ff42751c2dcaa9272900ce6a4dbe52381a
linghaitian <Xhmh123456>
2021-01-06 15:56:12 +0800

    增加neo4j-starter，mybatis-plus分页支持neo4j

---
+++ README.md
@@ -0,0 +1,3 @@
+### 简介
+整合了climb中的一些基础模块,主要是climb-mybatis-starter的使用，
+并且实现gateway需要的用户信息获取接口
--- src/main/java/com/example/climb/user/controller/TestController.java
+++ src/main/java/com/example/climb/user/controller/TestController.java
@@ -9,9 +9,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -48,7 +46,6 @@ public class TestController {
             @ApiImplicitParam(value = "test name",required = true, dataTypeClass=String.class),
     })
     @GetMapping("save")
-    @Transactional
     public Result<Boolean> save(Integer id,String name){
         Test test = new Test();
         test.setId(id);
