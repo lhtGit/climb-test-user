bde657b656201e3655d1cb7bb6cc25cb9c1c2e0f
linghaitian <Xhmh123456>
2021-01-13 18:25:38 +0800

    neo4j 合并seata没有完成 打算将climb-gateway封装成一个架子，尝试着写一个，目前遇到没变法对接口bean反序列化，打算整一个配置类实现这个东西

--- src/main/java/com/example/climb/user/controller/UserController.java
+++ src/main/java/com/example/climb/user/controller/UserController.java
@@ -3,12 +3,19 @@ package com.example.climb.user.controller;
 import com.climb.common.bean.Result;
 import com.climb.common.user.UserBaseInfo;
 import com.climb.common.user.auth.LoginUserInfo;
+import com.climb.common.user.bean.UserInfoDetails;
+import com.climb.common.user.bean.base.DefaultMenuInfo;
+import com.climb.common.user.bean.base.DefaultResourceInfo;
+import com.climb.common.user.bean.base.DefaultRoleinfo;
+import com.climb.common.user.bean.base.DefaultUserInfoDetails;
 import com.climb.common.util.ResultUtil;
 import com.example.climb.user.vo.AuthorityInfoRes;
 import com.example.climb.user.vo.UserDetailsRes;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 /**
  * @author lht
@@ -26,22 +33,36 @@ public class UserController {
      * @param loginUserInfo
      */
     @PostMapping("login")
-    public Result<UserDetailsRes> loginAndUserDetails(@RequestBody LoginUserInfo loginUserInfo){
+    public Result<UserInfoDetails> loginAndUserDetails(@RequestBody LoginUserInfo loginUserInfo){
         log.info("loginAndUserDetails:{}",loginUserInfo);
 
-        //用户基础信息
-        UserBaseInfo userBaseInfo = new UserBaseInfo();
-        userBaseInfo.setId("333");
-        userBaseInfo.setEmail("xxxx@qq.com");
-        userBaseInfo.setName("xxx");
-        userBaseInfo.setUsername("xxxUsername");
-        //装配用户详情
-        UserDetailsRes userDetailsRes = new UserDetailsRes();
-        //用户权限
-        userDetailsRes.setAuthoritys(Lists.newArrayList("1","2","3"));
-        userDetailsRes.setUserBaseInfo(userBaseInfo);
+        DefaultUserInfoDetails userInfoDetails = new DefaultUserInfoDetails();
+        DefaultMenuInfo defaultMenuInfo = new DefaultMenuInfo();
+        defaultMenuInfo.setIcon("icon");
+        defaultMenuInfo.setId("1");
+        defaultMenuInfo.setName("menu name");
+        defaultMenuInfo.setParentId("0");
+        userInfoDetails.setMenuInfo(Lists.newArrayList(defaultMenuInfo));
 
-        return ResultUtil.success(userDetailsRes);
+        DefaultResourceInfo resourceInfo = new DefaultResourceInfo();
+        resourceInfo.setId("1");
+        resourceInfo.setMethod("GET");
+        resourceInfo.setName("resource name");
+        resourceInfo.setPath("/test");
+        userInfoDetails.setResourceInfo(Lists.newArrayList(resourceInfo));
+
+        DefaultRoleinfo roleinfo = new DefaultRoleinfo();
+        roleinfo.setId("1");
+        roleinfo.setName("role name");
+        userInfoDetails.setRoleInfo(Lists.newArrayList(roleinfo));
+
+        userInfoDetails.setId("1");
+        userInfoDetails.setMail("test@qq.com");
+        userInfoDetails.setName("admin nick");
+        userInfoDetails.setPhone("1531515531");
+        userInfoDetails.setUsername("admin");
+        userInfoDetails.setGender(1);
+        return ResultUtil.success(userInfoDetails);
     }
 
 
